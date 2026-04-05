ca2f3e888b33285d94a0b40f8d873705efffd090
Mohamed Amine <143552006+MohAmine2006@users.noreply.github.com>
2026-04-05 14:21:31 -0400

    Add files via upload
    Database with all tables, but the Enum for status still gets me an error so I didnt add it. If anyone knows how to add the Enum, please add it to the tables.

---
+++ Deliverables/ecomproject (1).sql
@@ -0,0 +1,232 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 05, 2026 at 08:20 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ecomproject`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `booking`
+--
+
+CREATE TABLE `booking` (
+  `bookingId` int(10) NOT NULL,
+  `userId` int(10) NOT NULL,
+  `bookingRef` int(10) NOT NULL,
+  `date` date NOT NULL,
+  `totalPrice` double(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `booking_ticket`
+--
+
+CREATE TABLE `booking_ticket` (
+  `bookingTicketId` int(10) NOT NULL,
+  `bookingId` int(10) NOT NULL,
+  `ticketId` int(10) NOT NULL,
+  `pricePaid` double(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `event`
+--
+
+CREATE TABLE `event` (
+  `eventId` int(10) NOT NULL,
+  `title` varchar(20) NOT NULL,
+  `artist` varchar(50) NOT NULL,
+  `description` varchar(100) DEFAULT NULL,
+  `venueId` int(10) NOT NULL,
+  `date` date NOT NULL,
+  `startTime` varchar(15) NOT NULL,
+  `endTime` varchar(15) NOT NULL,
+  `imagePath` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ticket`
+--
+
+CREATE TABLE `ticket` (
+  `ticketId` int(10) NOT NULL,
+  `eventId` int(10) NOT NULL,
+  `section` varchar(15) NOT NULL,
+  `rowLetter` varchar(1) NOT NULL,
+  `seatNumber` varchar(5) NOT NULL,
+  `price` double(10,2) NOT NULL,
+  `heldUntil` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `userId` int(10) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `twoFactor` varchar(6) NOT NULL,
+  `role` varchar(25) NOT NULL,
+  `created_at` varchar(20) NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `venue`
+--
+
+CREATE TABLE `venue` (
+  `venueId` int(10) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `address` varchar(50) NOT NULL,
+  `city` varchar(20) NOT NULL,
+  `capacity` int(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `booking`
+--
+ALTER TABLE `booking`
+  ADD PRIMARY KEY (`bookingId`),
+  ADD KEY `userFK` (`userId`);
+
+--
+-- Indexes for table `booking_ticket`
+--
+ALTER TABLE `booking_ticket`
+  ADD PRIMARY KEY (`bookingTicketId`),
+  ADD KEY `bookingFK` (`bookingId`),
+  ADD KEY `ticketFK` (`ticketId`);
+
+--
+-- Indexes for table `event`
+--
+ALTER TABLE `event`
+  ADD PRIMARY KEY (`eventId`),
+  ADD KEY `venueFK` (`venueId`);
+
+--
+-- Indexes for table `ticket`
+--
+ALTER TABLE `ticket`
+  ADD PRIMARY KEY (`ticketId`) USING BTREE,
+  ADD KEY `eventFK` (`eventId`) USING BTREE;
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`userId`);
+
+--
+-- Indexes for table `venue`
+--
+ALTER TABLE `venue`
+  ADD PRIMARY KEY (`venueId`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `booking`
+--
+ALTER TABLE `booking`
+  MODIFY `bookingId` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `booking_ticket`
+--
+ALTER TABLE `booking_ticket`
+  MODIFY `bookingTicketId` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `event`
+--
+ALTER TABLE `event`
+  MODIFY `eventId` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `ticket`
+--
+ALTER TABLE `ticket`
+  MODIFY `ticketId` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `userId` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `venue`
+--
+ALTER TABLE `venue`
+  MODIFY `venueId` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `booking`
+--
+ALTER TABLE `booking`
+  ADD CONSTRAINT `userFK` FOREIGN KEY (`userId`) REFERENCES `user` (`userId`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `booking_ticket`
+--
+ALTER TABLE `booking_ticket`
+  ADD CONSTRAINT `bookingFK` FOREIGN KEY (`bookingId`) REFERENCES `booking` (`bookingId`) ON DELETE CASCADE,
+  ADD CONSTRAINT `ticketFK` FOREIGN KEY (`ticketId`) REFERENCES `ticket` (`ticketId`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `event`
+--
+ALTER TABLE `event`
+  ADD CONSTRAINT `venueFK` FOREIGN KEY (`venueId`) REFERENCES `venue` (`venueId`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `ticket`
+--
+ALTER TABLE `ticket`
+  ADD CONSTRAINT `eventFK` FOREIGN KEY (`eventId`) REFERENCES `event` (`eventId`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
